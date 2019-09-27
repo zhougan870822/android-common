7d64e16216ef11f3f042b95dbffea279d0b03009
zhougan <564169392@qq.com>
2019-09-27 17:59:07 +0800

    1

--- android-common/src/main/java/com/zhoug/android/common/Constant.java
+++ android-common/src/main/java/com/zhoug/android/common/Constant.java
@@ -5,4 +5,9 @@ public class Constant {
      * 默认的FileProvider 的authority后缀
      */
     public static final String FILE_PROVIDER_AUTHORITY_SUFFIX=".fileprovider";
+
+    /**
+     * 手机存储的sdcard根目录
+     */
+    public static final String SDCARD_ROOT="/storage/emulated/0";
 }
